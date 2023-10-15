6cc4709bc61e3ca6bd92087f8feee9223d982e6d
SEOEUN <87869785+seoeunkong@users.noreply.github.com>
2023-10-15 11:41:29 +0900

    Moving Tile

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("# Game Object")]
+    public Player player;
+
+    void Awake()
+    {
+        instance = this; //자기자신으로 초기화
+    }
+
+}
---
+++ Assets/Scripts/Tile/Reposition.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        //플레이어 이동 방향 저장
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1; //대각선일때는 Normalized에 의해 1보다 작은 값이 됨
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+
+            case "Enemy":
+                if (coll.enabled)
+                {
+                    //플레이어의 이동 방향에 따라 맞은 편에서 등장하도록 이동
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+
+                }
+                break;
+        }
+    }
+}
